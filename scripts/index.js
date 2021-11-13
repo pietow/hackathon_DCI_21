98fd9da3f96e8a07df609f4558c45ba568c22052
pietow <pprotassow@posteo.de>
2021-11-13 17:00:34 +0100

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,69 @@
-// Any code you will write for your website (in the future!) should go here
-const burger = document.querySelector('.menu-btn')
-const closeBtn = document.querySelector('.menu-btn.open')
-const nav = document.querySelector('nav')
+//// Any code you will write for your website (in the future!) should go here
+//const burger = document.querySelector('.menu-btn')
+//const closeBtn = document.querySelector('.menu-btn.open')
+//const nav = document.querySelector('nav')
 
 
-let showNav = false
+//let showNav = false
 
-burger.addEventListener('click', (e) => {
-    if(!showNav) {
-        showNav = true
-        open(nav)
-        open(e.target)
-    }else {
-        showNav = false
-        close(nav)
-        close(e.target)
+//burger.addEventListener('click', (e) => {
+//    if(!showNav) {
+//        showNav = true
+//        open(nav)
+//        open(e.target)
+//    }else {
+//        showNav = false
+//        close(nav)
+//        close(e.target)
 
-    }
-})
+//    }
+//})
 
-function close() {
-    if(window.innerWidth >= 700) {
-        close(nav)
-        close(burger)
-    }
-}
+//function close() {
+//    if(window.innerWidth >= 700) {
+//        close(nav)
+//        close(burger)
+//    }
+//}
 
-// window.onresize = close;
+//// window.onresize = close;
 
-// burger.addEventListener('click', (e) => {
-//     open(nav)
-//     open(e.target)
-// })
+//// burger.addEventListener('click', (e) => {
+////     open(nav)
+////     open(e.target)
+//// })
 
 
 
-//####FUNCTION
-function open(ele) {
-    ele.classList.add('open')
-    if(ele.children.length !== 0) {
-        Array.from(ele.children).map(x => {
-            x.classList.add('open')
-            open(x)
-        })
-    }
-}
+////####FUNCTION
+//function open(ele) {
+//    ele.classList.add('open')
+//    if(ele.children.length !== 0) {
+//        Array.from(ele.children).map(x => {
+//            x.classList.add('open')
+//            open(x)
+//        })
+//    }
+//}
 
-function close(ele) {
-    ele.classList.remove('open')
-    if(ele.children.length !== 0) {
-        Array.from(ele.children).map(x => {
-            x.classList.remove('open')
-            close(x)
-        })
-    }
-}
+//function close(ele) {
+//    ele.classList.remove('open')
+//    if(ele.children.length !== 0) {
+//        Array.from(ele.children).map(x => {
+//            x.classList.remove('open')
+//            close(x)
+//        })
+//    }
+//}
+
+//#############MINION####
+
+const minion = document.querySelector('.chef')
+setTimeout(() => {
+        const oldTransform = minion.style.transform
+        minion.style.transform = 'translateX(0)'
+    setTimeout(() => {
+        minion.style.transform = oldTransform
+    }, 2000)
+}, 2000)
+
